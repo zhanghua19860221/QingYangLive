7bdf0faed8066c19b476e44c2e2f8be9163a2c85
张华 <3051942353@qq.com>
2020-12-29 14:56:38 +0800

    tabbar 创建 资源导入

--- QingYangLive/AppDelegate/AppDelegate.swift
+++ QingYangLive/AppDelegate/AppDelegate.swift
@@ -9,9 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    let TabbarVC = QYBaseTabBarController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = TabbarVC
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ QingYangLive/Mian/C/QYBaseNavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QYBaseNavigationViewController.swift
+//  QingYangLive
+//
+//  Created by 张华 on 2020/12/29.
+//
+
+import UIKit
+
+class QYBaseNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
